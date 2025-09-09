78d1b49c8ce2dff9daff47c28c696f584e1d544b
thomasr10 <t.rivoire01@gmail.com>
2025-09-09 20:00:29 +0200

    loader + start media query

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import MainButton from "./MainButton";
+import Loader from "./Loader";
 
 function ContactSection() {
 
@@ -8,6 +9,7 @@ function ContactSection() {
     const [subject, setSubject] = useState('');
     const [message, setMessage] = useState('');
     const [emailConfirmation, setEmailConfirmation] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         emailjs.init({
@@ -17,6 +19,7 @@ function ContactSection() {
 
     function sendEmail(e) {
         e.preventDefault();
+        setIsLoading(true);
         const params = {
             name: name,
             subject: subject,
@@ -31,47 +34,57 @@ function ContactSection() {
                 setEmailFrom('');
                 setSubject('');
                 setMessage('');
+                setIsLoading(false);
             })
-            .catche(() => {
+            .catch(() => {
                 setEmailConfirmation('Problème survenue à l\'envoie de l\'email');
             });
     }
     return (
-        <section className="contact-section">
-            <div className="raw-limit-size center">
-                <div>
-                    <h2>Me contacter</h2>
-                    <p>Email : <a href="mailto:t.rivoire01@gmail.com">t.rivoire01@gmail.com</a></p>
-                    <p className="mt-16">Pour plus d'informations : <a href="/CV/RIVOIRE_Thomas - CV.pdf" target="blank">Mon CV</a></p>
+        <>
+
+            <section className="contact-section">
+                <div className="raw-limit-size center">
+                    <div>
+                        <h2>Me contacter</h2>
+                        <p>Email : <a href="mailto:t.rivoire01@gmail.com">t.rivoire01@gmail.com</a></p>
+                        <p className="mt-16">Pour plus d'informations : <a href="/CV/RIVOIRE_Thomas - CV.pdf" target="blank">Mon CV</a></p>
+                    </div>
+                    <div className="form-container">
+                        <form onSubmit={sendEmail}>
+                            {
+                                (emailConfirmation !== '') && (
+                                    <div className="msg-container">{emailConfirmation}</div>
+                                )
+                            }
+                            <div className="input-container mt-32">
+                                <label htmlFor="name">Prénom et Nom</label>
+                                <input type="text" name="name" id="name" required placeholder="Prénom et Nom" value={name} onChange={(e) => { setName(e.target.value) }} />
+                            </div>
+                            <div className="input-container mt-24">
+                                <label htmlFor="mail">Adresse email</label>
+                                <input type="mail" name="mail" id="mail" required placeholder="Adresse email" value={emailFrom} onChange={(e) => { setEmailFrom(e.target.value) }} />
+                            </div>
+                            <div className="input-container mt-24">
+                                <label htmlFor="subject">Sujet</label>
+                                <input type="text" name="subject" id="subject" required placeholder="Sujet" value={subject} onChange={(e) => { setSubject(e.target.value) }} />
+                            </div>
+                            <div className="input-container mt-24">
+                                <label htmlFor="message">Message</label>
+                                <textarea name="message" id="message" required placeholder="Message" value={message} onChange={(e) => { setMessage(e.target.value) }}></textarea>
+                            </div>
+                            <MainButton children={"Envoyer"} type={'submit'} />
+                        </form>
+                    </div>
                 </div>
-                <div className="form-container">
-                    <form onSubmit={sendEmail}>
-                        {
-                            (emailConfirmation !== '') && (
-                                <div className="msg-container">{emailConfirmation}</div>
-                            )
-                        }
-                        <div className="input-container mt-32">
-                            <label htmlFor="name">Prénom et Nom</label>
-                            <input type="text" name="name" id="name" required placeholder="Prénom et Nom" value={name} onChange={(e) => { setName(e.target.value) }} />
-                        </div>
-                        <div className="input-container mt-24">
-                            <label htmlFor="mail">Adresse email</label>
-                            <input type="mail" name="mail" id="mail" required placeholder="Adresse email" value={emailFrom} onChange={(e) => { setEmailFrom(e.target.value) }} />
-                        </div>
-                        <div className="input-container mt-24">
-                            <label htmlFor="subject">Sujet</label>
-                            <input type="text" name="subject" id="subject" required placeholder="Sujet" value={subject} onChange={(e) => { setSubject(e.target.value) }} />
-                        </div>
-                        <div className="input-container mt-24">
-                            <label htmlFor="message">Message</label>
-                            <textarea name="message" id="message" required placeholder="Message" value={message} onChange={(e) => { setMessage(e.target.value) }}></textarea>
-                        </div>
-                        <MainButton children={"Envoyer"} type={'submit'} />
-                    </form>
-                </div>
-            </div>
-        </section>
+            </section>
+            {
+                isLoading === true && (
+                    <Loader />
+                )
+            }
+
+        </>
     )
 }
 
---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+function Loader() {
+    return (
+        <div className="loader-bg">
+            <div className="loader-container">
+                <div className="loader">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader;
